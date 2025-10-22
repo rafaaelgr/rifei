91afdbbb32f4fa30dc420827c465e3c5af7e5c18
RafaGarden01 <contato.rafaelgarden@gmail.com>
2025-10-22 15:54:37 -0300

    aaaa

---
+++ src/app/rifa/page.tsx
@@ -0,0 +1,10 @@
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export default function RifaPage() {
+    const router = useRouter();
+    useEffect(() => {
+        router.push("/12");
+    }, []);
+    return (<></>);
+}
